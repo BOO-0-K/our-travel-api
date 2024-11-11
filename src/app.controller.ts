082ad89deb5e459b7236f5e1455920260e99944c
BOO-0-K <boo970328@gmail.com>
2024-11-11 12:00:17 +0900

    [ADD] logger, httpException

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return '🚀 Service Working!';
+  }
+
+  @Get('/test/error')
+  getTestError(): string {
+    throw new HttpException(
+      '500 server error test',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './_commons/exceptions/httpException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
